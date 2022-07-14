af55b4288896fa1064ddcf8a2da1646a93aea38f
Vatsal Poddar <54811716+Vatsal562@users.noreply.github.com>
2022-07-14 22:20:32 +0530

    Create 1832. Check if the Sentence Is Pangram.cpp

---
+++ Codes/1832. Check if the Sentence Is Pangram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        unordered_set<char> s;
+        
+        for(auto x:sentence)
+            s.insert(x);
+        
+        if(s.size()==26) return true;
+        
+        return false;
+    }
+};
