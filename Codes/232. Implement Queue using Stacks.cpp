7e61b05fc55e1624b41e373c20789677a55bc343
Vatsal Poddar <54811716+Vatsal562@users.noreply.github.com>
2022-07-18 09:28:41 +0530

    Create 232. Implement Queue using Stacks.cpp

---
+++ Codes/232. Implement Queue using Stacks.cpp
@@ -0,0 +1,45 @@
+class MyQueue {
+public:
+    MyQueue() {
+        
+    }
+    
+    stack<int> s1,s2;
+    void push(int x) {
+        if(s1.empty()==true) s1.push(x);
+        else{
+            while(s1.empty()==false){
+                s2.push(s1.top());
+                s1.pop();
+            }
+            s1.push(x);
+            while(s2.empty()==false){
+                s1.push(s2.top());
+                s2.pop();
+            }
+        }
+    }
+    
+    int pop() {
+        int a = s1.top();
+        s1.pop();
+        return a;
+    }
+    
+    int peek() {
+        return s1.top();
+    }
+    
+    bool empty() {
+        return s1.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
