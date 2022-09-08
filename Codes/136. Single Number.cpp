41c721f1b2af898594fcee6ece5d060d0998b5b1
Vatsal Poddar <54811716+Vatsal562@users.noreply.github.com>
2022-09-08 12:27:10 +0530

    Create 136. Single Number.cpp

---
+++ Codes/136. Single Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int a=0;
+        
+        for(auto x:nums){
+            a = a ^ x;
+        }
+        
+        return a;
+        
+    }
+};
